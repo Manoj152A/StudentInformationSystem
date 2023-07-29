c4a473f21af0f8afc8bc87f9780569d9121c1df1
Manoj152A <manoj.chowdary0071@gmail.com>
2023-07-29 17:34:37 -0400

    loginpage is done and homepage

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../src/CSS/index.css"; // Update the import path for the index.css file
+import Login from "./Components/Login"; // Update the import path for the Login component
+import Homepage from "./Components/HomePage"; // Update the import path for the Homepage component
+import Home from "./Components/Home"; // Update the import path for the Home component
+import Forms from "./Components/Forms"; // Update the import path for the Forms component
+import ControlPanel from "./Components/ControlPanel"; // Update the import path for the ControlPanel component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Rest of the component code remains the same
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/homepage" element={<Homepage />}>
+          <Route index element={<Home />} />
+          <Route path="forms" element={<Forms />} />
+          <Route path="controlpanel" element={<ControlPanel />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
