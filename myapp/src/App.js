ec570acbc2f5f26f85b4ab86139750f8a424f127
AjayKumarNagula <nagulajay@gmail.com>
2023-08-01 12:16:20 -0400

    commiting user search page and user details page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainComponent from './main';
+import DetailsPage from './DetailsPage';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainComponent />} />
+        <Route path="/details/:detailsId" element={<DetailsPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
+
+
